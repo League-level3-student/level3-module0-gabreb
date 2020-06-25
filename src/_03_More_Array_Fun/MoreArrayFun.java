48826d3a13eca4225aae6a2a7a9b3148faa0da47
gabreb <62906858+gabreb@users.noreply.github.com>
2020-06-24 19:34:36 -0700

    hi

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -6,19 +6,39 @@ public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
 	
 	public static void main(String[] args) {
-		
+		String[]names = new String[5];
+		names[0] = "a";
+		names[1] = "b";
+		names[2] = "c";
+		names[4] = "e";
+		everyOther(names);
 	}
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	
-	void (String[] strings) {
-		
+	public static void Stringconverter(String[] strings) {
+		for (int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i]);
+		}
 	}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
-	
+	public static void reverseStringconverter(String[] strings) {
+		for (int i = strings.length-1; i>-1; i--) {
+			System.out.println(strings[i]);
+		}
+	}
+	public static void everyOther(String[] strings) {
+		for (int i = 0; i < strings.length; i++) {
+			if (i % 2 == 0) {
+				System.out.println(strings[i]);
+			}
+			else {
+				
+			}
+		}
+	}
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
 	
